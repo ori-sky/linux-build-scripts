e31d7a5b971e5fd0f6928c51513ae98079345f5b
Shockk <shokku.ra@gmail.com>
2015-05-09 18:42:13 +0000

    Add vim

--- host.sh
+++ host.sh
@@ -1,1 +1,1 @@
-apt-get -y install bash binutils bison bzip2 coreutils diffutils findutils gawk gcc grep gzip m4 make patch perl sed tar texinfo
+apt-get -y install vim bash binutils bison bzip2 coreutils diffutils findutils gawk gcc grep gzip m4 make patch perl sed tar texinfo
